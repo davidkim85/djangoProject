9fe6af61dfff74c998a44c88446ccdb65be5d48d
David Furman <poramok@gmail.com>
2023-05-23 23:49:01 +0300

    third commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG'))
+DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(filter(None, os.environ.get('ALLOWED_HOSTS','').split(','),))
