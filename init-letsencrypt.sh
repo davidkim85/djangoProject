fa64d6c4cbca7505c309ba0f37077b9e5c0de566
David Furman <poramok@gmail.com>
2023-05-25 09:32:40 +0300

    third commit

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=furmandavid.info www.furmandavid.info
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="poramok@gmail.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
