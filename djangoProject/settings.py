477ecbcdc3d6db63e301b25103ce262933cfcb29
David Furman <poramok@gmail.com>
2023-06-03 23:35:26 +0300

    third commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'blog','static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'blog','media')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
